729b83d7eb10778551c439cca6f8f7665c779b96
Zhong Zichen <zhongzichen1997@163.com>
2023-03-06 23:53:50 +0800

    🎉 Initial commit

---
+++ src/step-2.js
@@ -0,0 +1,55 @@
+// Step II: The render Function
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === "object" ? child : createTextElement(child)
+      ),
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+const Didact = {
+  createElement,
+};
+
+const element = Didact.createElement(
+  "div",
+  { id: "foo" },
+  Didact.createElement("a", null, "bar"),
+  Didact.createElement("b")
+);
+
+function ReactDOM() {}
+
+ReactDOM.render = render;
+
+function render(element, container) {
+  const dom =
+    element.type == "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
+  const isProperty = (key) => key !== "children";
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((name) => {
+      dom[name] = element.props[name];
+    });
+  element.props.children.forEach((child) => render(child, dom));
+  container.appendChild(dom);
+}
+
+const container = document.getElementById("root");
+ReactDOM.render(element, container);
